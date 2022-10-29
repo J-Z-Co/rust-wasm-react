5e2918a392a67f2b3e408e493d86ab6ae1ff5845
susii <qaz123zyy@gmail.com>
2022-10-29 19:47:26 +0800

    use the fixedBitSet to store cell status

--- rwr/src/lib.rs
+++ rwr/src/lib.rs
@@ -1,18 +1,21 @@
 mod utils;
 use std::fmt;
+extern crate js_sys;
+extern crate fixedbitset;
+use fixedbitset::FixedBitSet;
 
-impl fmt::Display for Universe{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead {'◻'} else {'◼'};
-                write!(f, "{}", symbol);
-            }
-            write!(f, "\n");
-        }
-        Ok(())
-    }
-}
+// impl fmt::Display for Universe{
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         for line in self.cells.as_slice().chunks(self.width as usize) {
+//             for &cell in line {
+//                 let symbol = if cell == Cell::Dead {'◻'} else {'◼'};
+//                 write!(f, "{}", symbol);
+//             }
+//             write!(f, "\n");
+//         }
+//         Ok(())
+//     }
+// }
 
 use wasm_bindgen::prelude::*;
 
@@ -44,7 +47,7 @@ pub enum Cell {
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: Vec<Cell>,
+    cells: FixedBitSet,
 }
 #[wasm_bindgen]//Public methods, exported to JS
 impl Universe {
@@ -55,19 +58,18 @@ impl Universe {
                 let idx = self.get_index(row, col);
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
-                let next_cell = match (cell, live_neighbors) {
+                next.set(idx, match (cell, live_neighbors) {
                     //rule 1 less than 2 neightbors lead to die
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (true, x) if x < 2 => false,
                     //rule 2 2 or 3 neibours let live the next gen
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (true, 2) | (true, 3) => true,
                     //rule 3 more than 3 neightbors lead to die
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (true, x) if x > 3 => false,
                     //rule 4 dead with 3 neightbors coms back to life
-                    (Cell::Dead, 3) => Cell::Alive,
+                    (false, 3) => true,
                     //all other cells remain in the same state
                     (otherwise, _) => otherwise,
-                };
-                next[idx] = next_cell;
+                });
             }
         }
         self.cells = next;
@@ -92,17 +94,15 @@ impl Universe {
         count
     }
     pub fn new() -> Universe{
-        let width = 128;
-        let height = 128;
+        let width = 64;
+        let height = 64;
 
-        let cells = (0..width*height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            }).collect();
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+        
+        for i in 0..size {
+            cells.set(i, if js_sys::Math::random() < 0.5 {false} else {true});
+        }
 
         Universe {
             width,
@@ -110,9 +110,9 @@ impl Universe {
             cells,
         }
     }
-    pub fn render(&self) -> String{
-        self.to_string()
-    }
+    // pub fn render(&self) -> String{
+    //     self.to_string()
+    // }
 
     //add more getter functions for width, height and pointer to cells array and expose to JS
     pub fn width(&self) -> u32 {
@@ -121,7 +121,7 @@ impl Universe {
     pub fn height(&self) -> u32 {
         self.height
     }
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
     }
 }
